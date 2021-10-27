0494136862ce09c2fc8223d361a6fb06846e0950
Yusuf Isbilir <yusufemirisbilir@gmail.com>
2021-10-27 14:26:55 +0300

    Scroll effects

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css"
 
 function Button({ imp, text, link }) {
   return (
-    <div className={`button ${imp == "secondary" ? "button__white" : ""}`}>
+    <div className={`button ${imp === "secondary" ? "button__white" : ""}`}>
       <a href={link}>{text}</a>
     </div>
   );
